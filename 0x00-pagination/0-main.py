5d635a55ddd90299dd6d9cd0fd83507c5be7c638
timmoh-king <timothykariuki43@gmail.com>
2023-10-23 20:57:09 +0300

    1-simple_pagination.py

--- 0x00-pagination/0-main.py
+++ 0x00-pagination/0-main.py
@@ -5,10 +5,6 @@ Main file
 
 index_range = __import__('0-simple_helper_function').index_range
 
-res = index_range(1, 7)
-print(type(res))
-print(res)
-
-res = index_range(page=3, page_size=15)
+res = index_range(page=1, page_size=10)
 print(type(res))
 print(res)
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,4 +40,15 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
+        """
+            Use assert to verify that both args are int > 0
+        """
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+
+        start, end = index_range(page, page_size)
+
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
