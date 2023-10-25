ebb752ca1e53b331470869fb4e29f236c7539271
timmoh-king <timothykariuki43@gmail.com>
2023-10-25 16:30:55 +0300

    0-basic_cache.py

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+    Create a class BasicCache that inherits from BaseCaching as caching system:
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+        class BasicCache
+    """
+    def __init__(self):
+        """ initialize the super class variables and constraints"""
+        super().__init__()
+
+    def put(self, key, item):
+        """
+            assign cache_data the item value for the key
+        """
+        if key or item is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+            return the value in cache_data linked to the key
+        """
+        return self.cache_data.get(key)
