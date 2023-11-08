573dcb2be57de4d5cda629feb005db9101f6c598
timmoh-king <timothykariuki43@gmail.com>
2023-11-08 09:35:08 +0300

    Task 3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -32,7 +32,7 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """render the 1-index.html page once the route is given"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
