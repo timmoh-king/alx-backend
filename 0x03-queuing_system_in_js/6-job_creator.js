517a576f5b25ec25ba341640003360036ab9a1e9
timmoh-king <timothykariuki43@gmail.com>
2023-11-28 21:58:35 +0300

    6-job_processor.js

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -2,25 +2,23 @@
  * In a file named 6-job_creator.js:
  */
 
-const kue = require('kue');
+import { createQueue } from 'kue';
 
-const queue = kue.createQueue();
+const queue = createQueue({name: 'push_notification_code'});
 
-const jobData = {
+const job = queue.create('push_notification_code', {
   phoneNumber: '4153518780',
   message: 'Account registered',
-};
-
-const newJob = queue.create('push_notification_code', jobData)
-  .priority('high')
-  .save();
-
-console.log(`Notification job created: ${newJob.id}`);
-
-newJob.on('complete', () => {
-  console.log('Notification job completed');
 });
 
-newJob.on('failed', () => {
-  console.log('Notification job failed');
-});
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
+job.save();
---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,16 @@
+/**
+ * In a file named 6-job_processor.js:
+ */
+
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}`, 'with message:', message);
+};
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
