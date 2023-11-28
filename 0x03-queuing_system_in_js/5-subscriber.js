918aaf57415eb4eb1cb436b2750975229dbe81c0
timmoh-king <timothykariuki43@gmail.com>
2023-11-28 20:33:26 +0300

    5-subscriber.js

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,25 @@
+/**
+ * In a file named 5-subscriber.js, create a redis client:
+ */
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connection', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log('Redis client not connected to the server:', error);
+});
+
+channel = 'holberton school channel';
+client.subscribe(channel);
+
+client.on('message', (_err, messsage) => {
+  console.log(messsage);
+  if (messsage === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+  }
+});
