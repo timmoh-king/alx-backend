c3e75a9a4cb5b5c4905be79ce9687ef75653d98e
timmoh-king <timothykariuki43@gmail.com>
2023-11-30 00:24:23 +0300

    7-job_processor.js

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,29 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+BLACKLISTED_NUMS = ['4153518780', '4153518781'];
+
+const sendNotification = (phoneNumber, message, job, done) => {
+  const total = 2; let pending = 2;
+  const sendInterval = setImmediate(() => {
+    if (total - pending <= total / 2) {
+      job.progress(total - pending, total);
+    }
+
+    if (BLACKLISTED_NUMS.includes(phoneNumber)) {
+      done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+      clearInterval(sendInterval);
+      return;
+    }
+
+    if (total === pending) {
+      console.log(`Sending notification to ${phoneNumber},`, `with message: ${message}`);
+    } --pending || done();
+    pending || clearInterval(sendInterval);
+  }, 1000);
+};
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
